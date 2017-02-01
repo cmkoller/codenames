d2babcc20ba4329c818cbf02557828697b03f882
Christina Koller <christinakoller@Christinas-MacBook-Air.local>
2017-01-31 19:42:01 -0500

    Mark game as started for all players when one player clicks start

--- react/src/containers/MainContainer.js
+++ react/src/containers/MainContainer.js
@@ -11,20 +11,37 @@ import TeamView from './../components/TeamView';
 import ChatView from './../components/ChatView';
 
 class MainContainer extends Component {
+  componentWillMount() {
+    this.pusher = new Pusher('a805a64f3dabd2cd9fae');
+    this.players = this.pusher.subscribe('games');
+  }
+
   componentDidMount() {
+    this._loadSessionPlayerInfo();
+    this._loadSessionGameStarted()
+
+    this.players.bind('game_started', function(){
+      this.props.loadStartedGame();
+    }, this);
+  }
+
+  _loadSessionGameStarted() {
+    let gameStarted = (sessionStorage.gameStarted === "true");
+
+    if(gameStarted) {
+      this.props.loadStartedGame();
+    }
+  }
+
+  _loadSessionPlayerInfo() {
     let username = sessionStorage.username;
     let team = sessionStorage.team;
     let role = sessionStorage.role;
     let ready = sessionStorage.ready;
     ready === "true" ? ready = true : ready = false
-    let gameStarted = sessionStorage.gameStarted;
-    gameStarted === "true" ? gameStarted = true : gameStarted = false
+
 
     this.props.loadUserData({username: username, team: team, role: role, ready: ready});
-
-    if(gameStarted) {
-      this.props.loadStartedGame();
-    }
   }
 
   render() {
